72a3a932ef5ebcdb1e877184a44315c2717a4117
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-07-28 18:56:55 +0530

    Add files via upload

---
+++ 800/dadd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
+
+int main() {
+
+    int x,y;
+    cin>>x>>y;
+
+    int sum = addNumbers(x,y);
+
+    cout << sum << "\n";
+
+
+    return 0;
+}
