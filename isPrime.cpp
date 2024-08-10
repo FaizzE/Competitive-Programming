ac634d56ac405a63a49a32d9f9af9024785f2e1b
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-08-10 21:29:21 +0530

    Add files via upload

---
+++ isPrime.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for(int i = 2; i <= n; i++) {
+        if(i % 2 == 1) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    
+
+    return 0;
+}
